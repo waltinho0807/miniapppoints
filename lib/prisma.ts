fbc696a9fd96655194049e3d0d20578a2542283a
waltinho0807 <waltinhocalegari@hotmail.com>
2025-09-14 22:25:52 -0300

    ultima de hoje

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,14 +1,12 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from "@prisma/client";
 
-declare global {
-  // eslint-disable-next-line no-var
-  var prisma: PrismaClient | undefined;
-}
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-export const prisma =
-  global.prisma ??
-  new PrismaClient({
-    log: ['query'], // opcional, remove em produção se quiser
-  });
+export const prisma = 
+    globalForPrisma.prisma || 
+    new PrismaClient({
+        log: ["query"]
+    });
 
-if (process.env.NODE_ENV !== 'production') global.prisma = prisma;
+if(process.env.NODE_ENV !== 'production')   globalForPrisma.prisma = prisma; 
+
